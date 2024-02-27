31fdbd33439f989d39d2c0de81324c5997172fc3
ar.rehman135 <abdulrehmanmirza135@gmail.com>
2024-02-27 16:03:19 +0500

    update base url

--- frontend/src/apis/api.ts
+++ frontend/src/apis/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_KEY = "NQFYOQ94IOYKV7IL";
 const ENTRY_LIST_END_POINT = "all_data/";
-const BASE_URL = "http://192.168.1.8:8000/api/";
+const BASE_URL = "http://localhost:8000/api/";
 
 export const fetchStockPrice = async (stockName: string) => {
   const response = await axios.get(
